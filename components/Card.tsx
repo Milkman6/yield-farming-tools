4a336585bc3917b2c0228d98a435a3fae9523089
Carter <carterm126@gmail.com>
2020-09-13 16:21:35 -0700

    card color fix

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,7 +1,15 @@
 import { Box } from '@chakra-ui/core'
 
 export const Card = ({ children, ...props }) => (
-  <Box rounded="lg" borderWidth="1px" borderRadius={18} m={4} p={4} {...props}>
+  <Box
+    rounded="lg"
+    borderWidth="1px"
+    backgroundColor="white"
+    borderRadius={18}
+    m={4}
+    p={4}
+    {...props}
+  >
     {children}
   </Box>
 )
